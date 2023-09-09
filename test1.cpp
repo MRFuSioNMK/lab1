a7d06a7a43b52aabd71a92a576c6d46e97827855
MRFuSioNMK <144415774+MRFuSioNMK@users.noreply.github.com>
2023-09-09 20:10:44 +0200

    Add files via upload

---
+++ test1.cpp
@@ -0,0 +1,12 @@
+#include <iostream> //i/o library
+using namespace std; //instead od prefix std::
+int main()//header
+{  //begin
+    float a, res; //declaration input
+    cout <<"a = ";//view text to console
+    cin >> a;//enter changes from the console
+    res = a + 5;//calculate the result
+    cout <<" a+5 = " <<res <<endl;//display the result to the console4
+    
+    return 0; //return to OS
+}  // end
