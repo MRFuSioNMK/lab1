775f45a55e21299e03cb7fe38677a7044ce28ad5
MRFuSioNMK <144415774+MRFuSioNMK@users.noreply.github.com>
2023-09-09 20:13:21 +0200

    Add files via upload

--- test1.cpp
+++ test1.cpp
@@ -7,6 +7,5 @@ int main()//header
     cin >> a;//enter changes from the console
     res = a + 5;//calculate the result
     cout <<" a+5 = " <<res <<endl;//display the result to the console4
-    
     return 0; //return to OS
 }  // end
